1e7f84f000b01d9b31049a4e8719c509d6843d3b
TatSPb <t-sverchkova@yandex.ru>
2023-06-25 00:03:16 +0300

    Initial commit

---
+++ src/main/java/cw2_2023_06/Cw2202306Application.java
@@ -0,0 +1,13 @@
+package cw2_2023_06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cw2202306Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cw2202306Application.class, args);
+    }
+
+}
---
+++ src/test/java/cw2_2023_06/Cw2202306ApplicationTests.java
@@ -0,0 +1,13 @@
+package cw2_2023_06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cw2202306ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
